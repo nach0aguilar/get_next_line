07af781cebd342263ba6228342473aa0571d960a
nach0aguilar <nachoaguilarfdz@gmail.com>
2023-10-16 21:43:12 +0200

    created structure

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: igaguila <igaguila@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/16 20:14:32 by igaguila          #+#    #+#             */
+/*   Updated: 2023/10/16 20:18:16 by igaguila         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdio.h>
+
+char    *get_next_line(int fd);
+
+#endif
+
